85630516c58ea149f8ba722b7affcb58d0581f97
Thanapat Suwannaard <thasuohm@gmail.com>
2021-02-11 00:01:56 +0700

    update layout

--- 62130500037_groupwork_3/main.js
+++ 62130500037_groupwork_3/main.js
@@ -4,7 +4,10 @@ const app = {
             gallery: [
                 {images: 'images/doraemon.png',name : 'Doraemon',fav: false},
                 {images: 'images/nobita.png', name : 'Nobita',fav: false},
-                {images: 'images/suneo.png',name : 'Suneo', fav: false}
+                {images: 'images/suneo.png',name : 'Suneo', fav: false},
+                {images: 'images/giant.png', name : 'Giant',fav: false},
+                {images: 'images/shizuka.jpg', name : 'Shizuka',fav: false},
+                {images: 'images/dekisugi.jpg', name : 'Dekisugi',fav: false},
                 ]
            ,favicon: 'images/favorite.svg'
         }
